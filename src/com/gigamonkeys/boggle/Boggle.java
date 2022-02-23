4ce42cb37fff021c8c3192a4898b66c6f7652c0d
Peter Seibel <peter@gigamonkeys.com>
2022-02-22 17:22:59 -0800

    Word points method.

--- src/com/gigamonkeys/boggle/Boggle.java
+++ src/com/gigamonkeys/boggle/Boggle.java
@@ -1,3 +1,25 @@
 package com.gigamonkeys.boggle;
 
-class Boggle {}
+import java.util.*;
+
+class Boggle {
+
+  // From https://www.hasbro.com/common/instruct/boggle.pdf
+  private static int[] scores = {1, 1, 2, 3, 5, 11};
+
+  private Set<String> words = new HashSet<String>();
+
+  Boggle(String wordsFile) {
+    // Open file and add words to words set.
+    words.add("food");
+    words.add("computer");
+  }
+
+  boolean isWord(String word) {
+    return words.contains(word);
+  }
+
+  int points(String word) {
+    return scores[Math.min(word.length(), 8) - 3];
+  }
+}
--- src/com/gigamonkeys/boggle/Main.java
+++ src/com/gigamonkeys/boggle/Main.java
@@ -3,6 +3,14 @@ package com.gigamonkeys.boggle;
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-    new UI().run();
+    //new UI().run();
+    var b = new Boggle("foo");
+    System.out.println(b.points("cow"));
+    System.out.println(b.points("fork"));
+    System.out.println(b.points("about"));
+    System.out.println(b.points("awhile"));
+    System.out.println(b.points("unhappy"));
+    System.out.println(b.points("whatever"));
+    System.out.println(b.points("extremely"));
   }
 }
