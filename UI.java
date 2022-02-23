cea1fce6464de14afbe5f3c1e33e8ef0230448db
Peter Seibel <peter@gigamonkeys.com>
2022-02-23 00:02:44 +0000

    Initial commit

---
+++ UI.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class UI {
+  public void run() {
+    JFrame f = new JFrame("Boggle");
+    var b = newButton("A");
+    f.add(b);// adding button in JFrame
+
+    f.setSize(400, 500);// 400 width and 500 height
+    f.setLayout(null);// using no layout managers
+    f.setVisible(true);// making the frame visible
+  }
+  private JButton newButton(String label) {
+    JButton b = new JButton(label);
+    b.setBounds(130, 100, 100, 40);
+    return b;
+  }
+}
